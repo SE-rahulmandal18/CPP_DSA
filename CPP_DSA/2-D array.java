49694cc606a6b1602ea950a41810bfbcaf3227e4
SE-rahulmandal18 <110009121+SE-rahulmandal18@users.noreply.github.com>
2026-05-18 12:45:03 +0530

    print sum of each row in 2-d array

--- CPP_DSA/2-D array.java
+++ CPP_DSA/2-D array.java
@@ -25,7 +25,27 @@ class Codechef
          }         
          return result; 
     }
-    
+
+	static List<Integer> colSum(int arr[][]){
+        
+        List<Integer> result = new ArrayList<>();
+        
+        int m = arr.length;
+        int n = arr[0].length;
+        
+        for(int col=0; col<n; col++){
+            
+            int sum = 0;
+            
+            for(int row=0; row<m; row++){
+                sum += arr[row][col];
+            }
+            
+            result.add(sum);
+        }
+        
+        return result;
+    }
 
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -37,6 +57,10 @@ class Codechef
         
         
         List<Integer> ans = rowSum(arr);
-        System.out.println("total sum: " +ans);       
+        System.out.println("total sum: " +ans);    
+
+		 
+        List<Integer> ans1 = colSum(arr);
+        System.out.println("col sum: " + ans1);
 	}
 }
